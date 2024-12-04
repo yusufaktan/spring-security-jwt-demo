5cf46801c54695ec1266a3c84d26b690e1dd9870
Yusuf Aktan <yusufaktn@outlook.com>
2024-12-05 02:15:06 +0300

    spring init

---
+++ src/main/java/com/aktanyusuf/spring_security_jwt_demo/SpringSecurityJwtDemoApplication.java
@@ -0,0 +1,13 @@
+package com.aktanyusuf.spring_security_jwt_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aktanyusuf/spring_security_jwt_demo/SpringSecurityJwtDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aktanyusuf.spring_security_jwt_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
