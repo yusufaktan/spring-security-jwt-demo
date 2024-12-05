87c802dcf7a0cdc3243f15a116dcc5aad3a6e691
Yusuf Aktan <yusufaktn@outlook.com>
2024-12-05 17:53:30 +0300

    add JwtAuthenticationFilter and AppConfig

---
+++ src/main/java/com/aktanyusuf/config/AppConfig.java
@@ -0,0 +1,51 @@
+package com.aktanyusuf.config;
+
+import com.aktanyusuf.model.User;
+import com.aktanyusuf.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Optional;
+
+@Configuration
+public class AppConfig {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                Optional<User> optional = userRepository.findByUsername(username);
+                if (optional.isPresent()){
+                    return optional.get();
+                }
+                return null;
+            }
+        };
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+
+        return authenticationProvider;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
